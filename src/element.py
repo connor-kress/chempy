29335a11459f5393d6c03d4352e1ebc6768de113
connor-kress <con2coool@gmail.com>
2023-09-28 19:54:32 -0400

    Created the `Element` class.

---
+++ src/element.py
@@ -0,0 +1,37 @@
+from data.elements import ATOMIC_NUMS
+from typing import Self
+import numpy as np
+
+
+class Element:
+    _all_elements: dict[str, Self] = {}
+
+    def __init__(self, symbol: str) -> None:
+        """This is ran when a new type of element is passed to `Element`.
+        
+        Takes the element `symbol` and derives other basic properties.
+        """
+        self.symbol = symbol
+        self.number = ATOMIC_NUMS[symbol]
+
+        temp = np.zeros(len(ATOMIC_NUMS), dtype=int)
+        temp[self.number-1] = 1
+        self.vector = temp
+    
+    def __new__(cls, symbol: str) -> Self:
+        """"Caches all `Element` instances to avoid redundant memory usage."""
+        if symbol in cls._all_elements.keys():
+            return cls._all_elements[symbol]
+
+        if symbol not in ATOMIC_NUMS.keys():
+            raise ValueError(f'"{symbol}" is not a recognized element.')
+        
+        element = super().__new__(cls)
+        cls._all_elements[symbol] = element
+        return element
+    
+    def __str__(self) -> str:
+        return self.symbol
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.symbol}')"
