a728d79da14078c7eca37231556a524c69273f8e
connor-kress <con2coool@gmail.com>
2023-09-29 15:30:15 -0400

    Added parsing to the `Compound` class.

--- src/compound.py
+++ src/compound.py
@@ -1,9 +1,12 @@
 from data.elements import ATOMIC_NUMS
 from collections import Counter
 from element import Element
+from typing import Self
 import numpy as np
 
 NUMBER_OF_ELEMENTS = len(ATOMIC_NUMS)
+LEFT_DELS = ['(', '[', '{']
+RIGHT_DELS = [')', ']', '}']
 
 
 class Compound:
@@ -45,3 +48,106 @@ class Compound:
                 f'Connot compare the types `Compound` and `{type(other).__name__}`'
             )
         return self.elements == other.elements
+
+    @staticmethod
+    def parse_from_string(compound_string) -> Self:
+        compound_string = compound_string.replace(' ', '')
+        if not all(
+            c.isalnum()
+                or c in LEFT_DELS
+                or c in RIGHT_DELS
+            for c in compound_string
+        ):
+            raise ValueError(
+                f'Invalid character found while parsing {compound_string}.'
+            )
+        if (sum(c in LEFT_DELS for c in compound_string)
+            != sum(c in RIGHT_DELS for c in compound_string)):
+            raise ValueError(
+                f'Unequal left and right delimiters in {compound_string}.'
+            )
+        
+        tokens = Compound._parse_to_tokens(compound_string)
+        elements = Compound._parse_from_tokens(tokens)
+
+        return Compound(elements, compound_string)
+    
+    @staticmethod
+    def _parse_to_tokens(compound_string: str) -> list[Element | str | int]:
+        tokens = []
+        num_str = ''
+        lower = ''
+        for c in reversed(compound_string):
+            if c.isdigit():
+                if lower:
+                    raise ValueError(
+                        f'Invalid input syntax ({compound_string})'
+                    )
+                num_str = c + num_str
+                continue
+            if num_str:
+                tokens.insert(0, int(num_str))
+                num_str = ''
+            if c in LEFT_DELS + RIGHT_DELS:
+                if lower:
+                    raise ValueError(
+                        f'Invalid input syntax ({compound_string})'
+                    )
+                tokens.insert(0, c)
+            elif c.islower():
+                lower = c + lower
+            elif lower:
+                tokens.insert(0, Element(c+lower))
+                lower = ''
+            else:
+                tokens.insert(0, Element(c))
+        if num_str or lower:
+            raise ValueError(f'Invalid input syntax "{compound_string}"')
+        return tokens
+
+    @staticmethod
+    def _parse_from_tokens(
+        tokens: list[Element | str | int]
+    ) -> Counter[Element]:
+        def get_closing_index(start: int, l_del: str) -> int:
+            r_del = RIGHT_DELS[LEFT_DELS.index(l_del)]
+            count = 0
+            for i in range(start, len(tokens)):
+                frag = tokens[i]
+                if frag == l_del:
+                    count += 1
+                elif frag == r_del:
+                    if count == 0:
+                        return i
+                    count -= 1
+            raise ValueError(
+                f'"{l_del}" opened at i={start-1} was never closed.'
+            )
+
+        elements = Counter()
+        i = 0
+        while i < len(tokens):
+            token = tokens[i]
+            if token in LEFT_DELS:
+                close_idx = get_closing_index(i+1, token)
+                inside = Compound._parse_from_tokens(tokens[i+1:close_idx])
+                if (close_idx + 1 <= len(tokens) - 1
+                    and isinstance(tokens[close_idx+1], int)):
+                    mul = tokens[close_idx+1]
+                    for e in inside.keys():
+                        inside[e] = inside[e] * mul
+                    elements += inside
+                    i = close_idx + 2
+                else:
+                    elements += inside
+                    i = close_idx + 1
+                continue
+            if token in RIGHT_DELS:
+                raise ValueError('Invalid equation syntax (delimeters).')
+            if isinstance(token, Element):  # element
+                if i+1 < len(tokens) and isinstance(tokens[i+1], int):
+                    elements[token] += tokens[i+1]
+                else:
+                    elements[token] += 1
+            i += 1
+        return elements
