a87a5fc2672955a601c2e9fbd427b125649c3dcb
connor-kress <con2coool@gmail.com>
2023-09-29 18:49:51 -0400

    Added `__init__.py` files.

---
+++ src/chempy/__init__.py
@@ -0,0 +1,12 @@
+from utils import float_gcd, solve
+from element import Element
+from compound import Compound
+from equation import Equation
+
+__all__ = [
+    Element,
+    Compound,
+    Equation,
+    float_gcd,
+    solve,
+]
---
+++ src/chempy/utils/__init__.py
@@ -0,0 +1,7 @@
+from gcd import float_gcd
+from solve_system import solve
+
+__all__ = [
+    float_gcd,
+    solve,
+]
