2af4a207d4586fbb2140640b3c755f02bec1915e
connor-kress <con2coool@gmail.com>
2023-09-28 21:29:48 -0400

    Switched order of `Element`'s `__new__` and `__init__` functions.

--- src/element.py
+++ src/element.py
@@ -8,18 +8,6 @@ NUMBER_OF_ELEMENTS = len(ATOMIC_NUMS)
 class Element:
     _all_elements: dict[str, Self] = {}
 
-    def __init__(self, symbol: str) -> None:
-        """This is ran when a new type of element is passed to `Element`.
-        
-        Takes the element `symbol` and derives other basic properties.
-        """
-        self.symbol = symbol
-        self.number = ATOMIC_NUMS[symbol]
-
-        temp = np.zeros(NUMBER_OF_ELEMENTS, dtype=int)
-        temp[self.number-1] = 1
-        self.vector = temp
-    
     def __new__(cls, symbol: str) -> Self:
         """"Caches all `Element` instances to avoid redundant memory usage."""
         if symbol in cls._all_elements.keys():
@@ -31,6 +19,18 @@ class Element:
         element = super().__new__(cls)
         cls._all_elements[symbol] = element
         return element
+    
+    def __init__(self, symbol: str) -> None:
+        """This is ran when a new type of element is passed to `Element`.
+        
+        Takes the element `symbol` and derives other basic properties.
+        """
+        self.symbol = symbol
+        self.number = ATOMIC_NUMS[symbol]
+
+        temp = np.zeros(NUMBER_OF_ELEMENTS, dtype=int)
+        temp[self.number-1] = 1
+        self.vector = temp
     
     def __str__(self) -> str:
         return self.symbol
